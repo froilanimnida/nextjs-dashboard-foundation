5d66f39849f0ff526b830401706ca5797d32a71b
froilaaaaan1 <froilanaquino1@gmail.com>
2024-05-05 16:20:40 +0800

    chapter 9 done

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -37,6 +37,8 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   noStore();
   try {
+    console.log('Fetching Latest Invoice Data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email
       FROM invoices
@@ -44,6 +46,7 @@ export async function fetchLatestInvoices() {
       ORDER BY invoices.date DESC
       LIMIT 5`;
 
+    console.log('Data fetch after 3 seconds');
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
